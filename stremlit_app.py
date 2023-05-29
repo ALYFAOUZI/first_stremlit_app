64052b149fdb85b226dd4b245a54daf09076f418
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-29 14:38:05 -0400

    Update stremlit_app.py
    Alhamdo le ALLAH incha ALLAH v03

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Dinner incha ALLAH')
 streamlit.header('Breakfast Menu Incha ALLAH')
 streamlit.text('🥣 Omega 3 Blueberry oatmeal incha ALLAH')
@@ -6,3 +7,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie incha ALLAH')
 streamlit.text('🐔 Hard-Boiled Free range-Egg incha ALLAH')
 streamlit.text('🥑 Avocado Toast incha ALLAH')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
