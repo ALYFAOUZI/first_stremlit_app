d1eeb6d7a846bf206ccc3a61afa77f474ecc785d
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-29 14:19:41 -0400

    Update stremlit_app.py
    Alhamdo le ALLAH 
    added build your own fruits

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner incha ALLAH')
 streamlit.header('Breakfast Menu Incha ALLAH')
-streamlit.text('Omega 3 Blueberry oatmeal incha ALLAH')
-streamlit.text('Kale, Spinach & Rocket Smoothie incha ALLAH')
-streamlit.text('Hard-Boiled Free range-Egg incha ALLAH')
+streamlit.text('🥣 Omega 3 Blueberry oatmeal incha ALLAH')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie incha ALLAH')
+streamlit.text('🐔 Hard-Boiled Free range-Egg incha ALLAH')
+streamlit.text('🥑 Avocado Toast incha ALLAH')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
