801eeb2fb7c6f584947541c0c3b678255b41c289
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-29 14:15:42 -0400

    Update stremlit_app.py
    Alhamdo le ALLAH

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner incha ALLAH')
+streamlit.header('Breakfast Menu Incha ALLAH')
+streamlit.text('Omega 3 Blueberry oatmeal incha ALLAH')
+streamlit.text('Kale, Spinach & Rocket Smoothie incha ALLAH')
+streamlit.text('Hard-Boiled Free range-Egg incha ALLAH')
