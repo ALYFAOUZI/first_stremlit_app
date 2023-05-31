8441b6e8b54e96e693013097b87e592f60d6fc53
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-31 10:11:43 -0400

    Update stremlit_app.py
    Alhamdo le ALLAH incha ALLAH v97

--- stremlit_app.py
+++ stremlit_app.py
@@ -18,10 +18,8 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice="Non"
-try:
-        fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if  fruit_choice=="Non":
+try:fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if  NOT fruit_choice:
         streamlit.error("Please select fruit to get information.")
   else:
  #streamlit.write('The user entered ', fruit_choice)
