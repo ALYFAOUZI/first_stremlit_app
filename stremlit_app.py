55a07b843231176583521532d423e94d8c02a41d
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-31 09:29:35 -0400

    Update stremlit_app.py
    Alhamdo le ALLAH incha ALLAH v71

--- stremlit_app.py
+++ stremlit_app.py
@@ -19,6 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 IF NOT fruit_choice:
   streamlit.error("Please select fruit to get information.")
