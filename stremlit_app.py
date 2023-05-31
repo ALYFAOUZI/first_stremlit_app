c60e3589446fc87d3010b9750ea1132777c4bf86
ALYFAOUZI <81492671+ALYFAOUZI@users.noreply.github.com>
2023-05-31 09:34:40 -0400

    Update stremlit_app.py
    alhamdo le ALLAH incha ALLAH v75

--- stremlit_app.py
+++ stremlit_app.py
@@ -24,8 +24,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 if not fruit_choice:
   streamlit.error("Please select fruit to get information.")
   else:
-    streamlit.error()
-streamlit.write('The user entered ', fruit_choice)
+ 
+#streamlit.write('The user entered ', fruit_choice)
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
